22ee64d32f1f5b67859886f5d76ec4414cd4c0be
limhaekyu <limk9500@gmail.com>
2022-07-17 14:25:18 +0900

    feat: create WebSecurityConfig

---
+++ src/main/java/com/example/springshop/process/domain/product/dto/response/SearchProductByKeywordResponseDto.java
@@ -0,0 +1,15 @@
+package com.example.springshop.process.domain.product.dto.response;
+
+import com.example.springshop.process.domain.product.domain.Product;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class SearchProductResponseDto {
+    private List<Product> productList;
+}
