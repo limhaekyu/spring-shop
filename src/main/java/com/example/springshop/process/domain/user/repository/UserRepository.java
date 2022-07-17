f1935a7c45a016d82d74cfe98a9bcb824b03f64c
limhaekyu <limk9500@gmail.com>
2022-07-17 15:23:10 +0900

    feat: add findByEmail
    - security에서 email을 받아와서 인증하는 단계에 필요하므로 추가

--- src/main/java/com/example/springshop/process/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/springshop/process/domain/user/repository/UserRepository.java
@@ -3,6 +3,10 @@ package com.example.springshop.process.domain.user.repository;
 import com.example.springshop.process.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUserNameAndPhoneNumber(String userName, String phoneNumber);
+
+    Optional<User> findByEmail(String username);
 }
