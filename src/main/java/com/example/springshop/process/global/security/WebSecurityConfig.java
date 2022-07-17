88abe1292bcc4ed68dd61043140943faca1d9b9d
limhaekyu <limk9500@gmail.com>
2022-07-17 15:06:52 +0900

    feat: add passwordEncoder and authentication, JwtToken setting

--- src/main/java/com/example/springshop/process/global/security/WebSecurityConfig.java
+++ src/main/java/com/example/springshop/process/global/security/WebSecurityConfig.java
@@ -1,19 +1,48 @@
 package com.example.springshop.process.global.security;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@RequiredArgsConstructor
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final JwtTokenProvider jwtTokenProvider;
+
+    // 암호화에 필요한 PasswordEncoder를 Bean 등록
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    // authenticationManager를 Bean 등록
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception{
+        return super.authenticationManagerBean();
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception{
-        http.httpBasic()
+        http.httpBasic().disable() // rest api를 고려해 기본 설정을 해제
+                .csrf().disable() // csrf 보안 토큰 설정 헤제
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // JWT토큰 기반 인증 -> 세션 사용하지 않는다.
                 .and()
-                .authorizeRequests()
+                .authorizeRequests() // 요청에 대한 권한 체크
                 .antMatchers("/admin/**").hasRole("ADMIN")
                 .antMatchers("/user/**").hasRole("USER")
-                .antMatchers("/**").permitAll();
+                .antMatchers("/**").permitAll() // 그외 나머지 요청은 누구나 접근 가능
+                .and()
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
+                        UsernamePasswordAuthenticationFilter.class);
+                // JwtAuthenticationFilter를 UsernamePasswordAuthenticationFilter 전에 넣는다.
     }
 }
