c4839720a2d4d760ad16378197c866db4eeaccbc
limhaekyu <limk9500@gmail.com>
2022-07-28 22:18:38 +0900

    feat: index.html을 메인화면으로 사용할 수 있게 설정
    - dependency에 thymeleaf 추가

--- src/main/java/com/example/springshop/process/global/security/WebSecurityConfig.java
+++ src/main/java/com/example/springshop/process/global/security/WebSecurityConfig.java
@@ -3,7 +3,9 @@ package com.example.springshop.process.global.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -19,30 +21,38 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     // 암호화에 필요한 PasswordEncoder를 Bean 등록
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
     // authenticationManager를 Bean 등록
     @Bean
     @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception{
+    public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception{
-        http.httpBasic().disable() // rest api를 고려해 기본 설정을 해제
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/resources/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+
+                .httpBasic().disable() // rest api를 고려해 기본 설정을 해제
                 .csrf().disable() // csrf 보안 토큰 설정 헤제
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // JWT토큰 기반 인증 -> 세션 사용하지 않는다.
                 .and()
-                .authorizeRequests() // 요청에 대한 권한 체크
+                .authorizeRequests()
                 .antMatchers("/admin/**").hasRole("ADMIN")
                 .antMatchers("/user/**").hasRole("USER")
                 .antMatchers("/**").permitAll() // 그외 나머지 요청은 누구나 접근 가능
                 .and()
                 .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
                         UsernamePasswordAuthenticationFilter.class);
-                // JwtAuthenticationFilter를 UsernamePasswordAuthenticationFilter 전에 넣는다.
+        // JwtAuthenticationFilter를 UsernamePasswordAuthenticationFilter 전에 넣는다.
     }
+
 }
